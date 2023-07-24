399c11e56ac8b8754123b8c46185631cabd9f9ae
gabrielrcruz <56709233+gabrielrcruz@users.noreply.github.com>
2023-07-24 15:20:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,48 @@
-let xPos = 0;
+const track = document.getElementById("image-track");
 
-gsap.timeline()
-    .set('.ring', { rotationY:180, cursor:'grab' }) //set initial rotationY so the parallax jump happens off screen
-    .set('.img',  { // apply transform rotations to each image
-      rotateY: (i)=> i*-36,
-      transformOrigin: '50% 50% 500px',
-      z: -500,
-      backgroundImage:(i)=>'url(https://picsum.photos/id/'+(i+32)+'/600/400/)',
-      backgroundPosition:(i)=>getBgPos(i),
-      backfaceVisibility:'hidden'
-    })    
-    .from('.img', {
-      duration:1.5,
-      y:200,
-      opacity:0,
-      stagger:0.1,
-      ease:'expo'
-    })
-    .add(()=>{
-      $('.img').on('mouseenter', (e)=>{
-        let current = e.currentTarget;
-        gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
-      })
-      $('.img').on('mouseleave', (e)=>{
-        gsap.to('.img', {opacity:1, ease:'power2.inOut'})
-      })
-    }, '-=0.5')
-
-$(window).on('mousedown touchstart', dragStart);
-$(window).on('mouseup touchend', dragEnd);
-      
-
-function dragStart(e){ 
-  if (e.touches) e.clientX = e.touches[0].clientX;
-  xPos = Math.round(e.clientX);
-  gsap.set('.ring', {cursor:'grabbing'})
-  $(window).on('mousemove touchmove', drag);
+const handleOnDown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
 }
 
-
-function drag(e){
-  if (e.touches) e.clientX = e.touches[0].clientX;    
-
-  gsap.to('.ring', {
-    rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
-    onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBgPos(i) }) }
-  });
+const handleOnMove = (e) => {
+  // Ignore when no mouse down (location is 0 when not clicking)
+  if(track.dataset.mouseDownAt === "0") return;
   
-  xPos = Math.round(e.clientX);
+  // How far our mouse has moved (using custom data property)
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  // The total range of drag (half the width of window)
+  const maxDelta = window.innerWidth / 2;
+  // Calculating the percentage of the distance traveled relative to range
+  const percentage = (mouseDelta / maxDelta) * -100;
+  // Adding current mouse drag to any previous drag
+  const newPercentageSansConstraints = parseFloat(track.dataset.prevPercentage) + percentage;
+  // Setting upper & lower constraints of 0 & 100%
+  const newPercentage = Math.max(Math.min(newPercentageSansConstraints, 0), -100);
+  
+  // Updating new drag percentage data prop
+  track.dataset.percentage = newPercentage;
+  // Updating carousel position using animate to give smooth scroll feel
+  track.animate({
+    transform: `translate(${newPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  // Updating each individual image position to pan left using animate
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${newPercentage + 100}% 50%`
+    }, { duration: 1200, fill: "forwards" });
+  }
 }
 
-
-function dragEnd(e){
-  $(window).off('mousemove touchmove', drag);
-  gsap.set('.ring', {cursor:'grab'});
+const handleOnUp = (e) => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
 }
 
+window.onmousedown = (e) => handleOnDown(e);
+window.ontouchstart = (e) => handleOnDown(e.touches[0]);
 
-function getBgPos(i){ //returns the background-position string to create parallax movement in each image
-  return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
-}
+window.onmousemove = (e) => handleOnMove(e);
+window.ontouchmove = (e) => handleOnMove(e.touches[0]);
+
+window.onmouseup = (e) => handleOnUp(e);
+window.ontouchend = (e) => handleOnUp(e.touches[0]);
